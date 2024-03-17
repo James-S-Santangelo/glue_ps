9d8b94d5edeb3d53ca0670c1ee11a0e2ec78f3c4
James Santangelo <james.santangelo37@gmail.com>
2024-03-16 22:28:01 -0400

    Gereating PicMin data for thetas as well

--- workflow/rules/picmin.smk
+++ workflow/rules/picmin.smk
@@ -13,28 +13,32 @@ rule generate_picmin_data:
     input:
         flag = rules.install_picmin_dependencies.output,
         fst = expand(rules.windowed_fst.output, city=CITIES, hab_comb=['urban_rural'], chrom=CHROMOSOMES), 
+        thetas = expand(rules.windowed_theta.output, city=CITIES, habitat=['urban', 'rural'], chrom=CHROMOSOMES), 
     output:
-        fst = f"{ANALYSIS_DIR}/picmin/csv/all_windowed_fst.csv",
-        hist = f"{ANALYSIS_DIR}/picmin/figures/missing_data_histogram.pdf"
+        stats = f"{ANALYSIS_DIR}/picmin/csv/all_windowed_stats.csv",
+        hist_fst = f"{ANALYSIS_DIR}/picmin/figures/missing_data_histogram_fst.pdf",
+        hist_tp = f"{ANALYSIS_DIR}/picmin/figures/missing_data_histogram_tp.pdf",
+        hist_td = f"{ANALYSIS_DIR}/picmin/figures/missing_data_histogram_td.pdf"
     conda: '../envs/picmin.yaml'
     script:
         "../scripts/r/generate_picmin_data.R"
 
-rule run_picmin:
-    input:
-        flag = rules.install_picmin_dependencies.output,
-        fst = rules.generate_picmin_data.output.fst
-    output:
-        f"{ANALYSIS_DIR}/picmin/csv/outliers/outliers_{{n}}_cities.csv",
-        f"{ANALYSIS_DIR}/picmin/figures/manhattan/picmin_manhattan_plot_{{n}}_cities.pdf",
-        f"{ANALYSIS_DIR}/picmin/figures/outlier_histograms/outliers_histogram_{{n}}_cities.pdf"
-    conda: '../envs/picmin.yaml'
-    script:
-        "../scripts/r/run_picmin.R"
+# rule run_picmin:
+#     input:
+#         flag = rules.install_picmin_dependencies.output,
+#         fst = rules.generate_picmin_data.output.fst
+#     output:
+#         f"{ANALYSIS_DIR}/picmin/csv/outliers/outliers_{{n}}_cities.csv",
+#         f"{ANALYSIS_DIR}/picmin/figures/manhattan/picmin_manhattan_plot_{{n}}_cities.pdf",
+#         f"{ANALYSIS_DIR}/picmin/figures/outlier_histograms/outliers_histogram_{{n}}_cities.pdf"
+#     conda: '../envs/picmin.yaml'
+#     script:
+#         "../scripts/r/run_picmin.R"
 
 rule picmin_done:
     input:
-        expand(rules.run_picmin.output, n=[i for i in range(10, 27)])
+        # expand(rules.run_picmin.output, n=[i for i in range(10, 27)])
+        expand(rules.generate_picmin_data.output)
     output:
         f"{ANALYSIS_DIR}/picmin/picmin.done"
     shell:
--- workflow/scripts/r/generate_picmin_data.R
+++ workflow/scripts/r/generate_picmin_data.R
@@ -18,32 +18,64 @@ load_windowed_fst <- function(path){
 
 }
 
-# Load per-city windowed fst, concatenate, and write
-windowed_fst_list <- snakemake@input[["fst"]]
-all_windowed_fst <- windowed_fst_list %>%
-    purrr::map_dfr(., load_windowed_fst) %>%
+# Function to load per-city windowed thetas
+load_windowed_thetas <- function(path){
+
+    city <- str_extract(basename(path), "^\\w+(?=_Chr)")
+    habitat <- str_extract(basename(path), "(?<=allSites_)\\w+(?=_win)")
+    df <- suppressMessages(read_delim(path, delim = '\t')) %>%
+        rename(region = 1,
+               "nSites_theta" = "nSites") %>%
+        mutate(city = city, habitat = habitat) %>%
+        mutate(tp_scaled = tP / nSites_theta) %>%
+        mutate(Tajima = ifelse(nSites_theta == 0, NA, Tajima)) %>%
+        dplyr::select(region, Chr, WinCenter, tp_scaled, Tajima, nSites_theta, city, habitat)
+    return(df)
+
+}
+
+# Load per-city windowed fst
+all_windowed_fst <- snakemake@input[["fst"]] %>%
+    purrr::map_dfr(., load_windowed_fst, .progress = TRUE) %>%
     mutate(region = str_extract(region, pattern = "(?<=\\()\\d+,\\d+(?=\\)$)")) %>% 
     separate(region, into = c("start", "end"), sep = ",")
 
-write_csv(all_windowed_fst, snakemake@output[[1]])
+# Load per-city windowed thetas
+all_windowed_thetas <- snakemake@input[["thetas"]] %>%
+    purrr::map_dfr(., load_windowed_thetas, .progress = TRUE) %>%
+    mutate(region = str_extract(region, pattern = "(?<=\\()\\d+,\\d+(?=\\)$)")) %>% 
+    separate(region, into = c("start", "end"), sep = ",")
+
+all_windowed_thetas_wide <- all_windowed_thetas %>%
+    pivot_wider(values_from = c("Tajima", "nSites_theta", "tp_scaled"), names_from = "habitat")
+
+all_windowed_stats <- all_windowed_thetas_wide %>%
+    left_join(., all_windowed_fst, by = c("Chr", "start", "end", "WinCenter", "city")) %>%
+    mutate(winID = paste0(Chr, ":", WinCenter),
+           delta_tp_ur = tp_scaled_urban - tp_scaled_rural,
+           delta_td_ur = Tajima_urban - Tajima_rural)
+
+write_csv(all_windowed_stats, snakemake@output[["stats"]])
 
 # Generate subsetted datasets with empirical P-values from Picmin
-all_cities_chr1 <- all_windowed_fst %>%
-    mutate(winID = paste0(Chr, ":", WinCenter)) %>%
+all_cities_df <- all_windowed_stats %>%
     group_by(city) %>%
-    mutate(emp = PicMin:::EmpiricalPs(fst, large_i_small_p = TRUE)) %>%
-    dplyr::select(city, winID, emp)
+    mutate(emp_fst = PicMin:::EmpiricalPs(fst, large_i_small_p = TRUE),
+           emp_tp = PicMin:::EmpiricalPs(delta_tp_ur, large_i_small_p = TRUE),
+           emp_td = PicMin:::EmpiricalPs(delta_td_ur, large_i_small_p = TRUE)) %>%
+    dplyr::select(city, winID, emp_fst, emp_tp, emp_td)
 
-################################
-#### MISSING DATA HISTOGRAM ####
-################################
+#################################
+#### MISSING DATA HISTOGRAMS ####
+#################################
 
-all_cities_chr1_wide <- all_cities_chr1 %>%
-    spread(key = city, value = emp) %>%
+all_cities_wide <- all_cities_df %>%
+    dplyr::select(city, winID, emp_fst) %>%
+    spread(key = city, value = emp_fst) %>%
     column_to_rownames("winID")
 
-column_names <- colnames(all_cities_chr1_wide)
-missing_data_histogram <- all_cities_chr1_wide %>%
+column_names <- colnames(all_cities_wide)
+missing_data_histogram <- all_cities_wide %>%
     rowwise() %>%
     mutate(na_count = sum(is.na(c_across(all_of(column_names))))) %>%
     ggplot(aes(x = na_count)) +
@@ -54,7 +86,59 @@ missing_data_histogram <- all_cities_chr1_wide %>%
           axis.text = element_text(size = 12))
 
 ggsave(
-    filename = snakemake@output[[2]],
+    filename = snakemake@output[["hist_fst"]],
+    plot = missing_data_histogram,
+    device = "pdf",
+    width = 10,
+    height = 10,
+    units = "in",
+    dpi = 600
+)
+
+all_cities_wide <- all_cities_df %>%
+    dplyr::select(city, winID, emp_tp) %>%
+    spread(key = city, value = emp_tp) %>%
+    column_to_rownames("winID")
+
+column_names <- colnames(all_cities_wide)
+missing_data_histogram <- all_cities_wide %>%
+    rowwise() %>%
+    mutate(na_count = sum(is.na(c_across(all_of(column_names))))) %>%
+    ggplot(aes(x = na_count)) +
+    geom_histogram(bins = 26, color = "black", fill = "white") +
+    ylab("Number of genomic windows") + xlab("Number of cities with missing data") +
+    theme_classic() +
+    theme(axis.title = element_text(size = 14),
+          axis.text = element_text(size = 12))
+
+ggsave(
+    filename = snakemake@output[["hist_tp"]],
+    plot = missing_data_histogram,
+    device = "pdf",
+    width = 10,
+    height = 10,
+    units = "in",
+    dpi = 600
+)
+
+all_cities_wide <- all_cities_df %>%
+    dplyr::select(city, winID, emp_td) %>%
+    spread(key = city, value = emp_td) %>%
+    column_to_rownames("winID")
+
+column_names <- colnames(all_cities_wide)
+missing_data_histogram <- all_cities_wide %>%
+    rowwise() %>%
+    mutate(na_count = sum(is.na(c_across(all_of(column_names))))) %>%
+    ggplot(aes(x = na_count)) +
+    geom_histogram(bins = 26, color = "black", fill = "white") +
+    ylab("Number of genomic windows") + xlab("Number of cities with missing data") +
+    theme_classic() +
+    theme(axis.title = element_text(size = 14),
+          axis.text = element_text(size = 12))
+
+ggsave(
+    filename = snakemake@output[["hist_td"]],
     plot = missing_data_histogram,
     device = "pdf",
     width = 10,
